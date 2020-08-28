7c86aaf7de2eb3e1572d3a4a5d1ee952f402a5ce
sungmin69355 <sungmin69355@naver.com>
2020-08-28 15:53:25 +0900

    안심도서대출 수정

--- gdlibrary.py
+++ gdlibrary.py
@@ -1,36 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import datetime
-
 
 def findpeople():
-    
     driver = webdriver.Chrome('/Users/sungmin/chromedriver')
     driver.get('http://www.gdlibrary.or.kr/gilib/1000658/100049/bbsList.do')
-    
+
     url = 'http://www.gdlibrary.or.kr/gilib/1000658/100049/bbsList.do'
     req = requests.get(url)
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
 
-    nowday = "2020-08-27"
+    nowday = "2020-08-28"
     rank= 0
+    ##첫페이지
     for tag in soup.select('td'):
         if tag.text == nowday:
             rank=rank+1
-
+    ##두번쨰페이지
     driver.find_element_by_xpath("""//*[@id="listForm"]/div[2]/p/span[2]/a""").click()
     for tag in soup.select('td'):
         if tag.text == nowday:
             rank=rank+1
+    ##세번째페이지
+    driver.find_element_by_xpath("""//*[@id="listForm"]/div[2]/p/span[6]/a""").click()
+    for tag in soup.select('td'):
+        if tag.text == nowday:
+            rank=rank+1
 
-    for page in range(4,14):
-        driver.find_element_by_xpath("""//*[@id="listForm"]/div[2]/p/span[""" +str(page)+"""]/a""").click()
-        for tag in soup.select('td'):
-            if tag.text == nowday:
-                rank=rank+1
-    print("어제의 안심도서대출은 "+str(rank) +"명 입니다.")
+    print("오늘의 안심도서대출은 "+str(rank-1) +"명 입니다.")
 
 
 findpeople()
