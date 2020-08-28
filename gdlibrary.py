6773f3e6d5de6c2edf402dd5c22dc714383dc9a6
sungmin69355 <sungmin69355@naver.com>
2020-08-28 11:05:50 +0900

    안심도서대출인원 크롤링

--- gdlibrary.py
+++ gdlibrary.py
@@ -3,27 +3,50 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import datetime
 
-#driver = webdriver.Chrome(executable_path='c:/Users/sungmin/chromedriver')
-#driver.get('https://www.naver.com (https://www.naver.com/)')
 
-url = 'http://www.gdlibrary.or.kr/gilib/1000658/100049/bbsList.do'
-req = requests.get(url)
-html = req.text
-soup = BeautifulSoup(html, 'html.parser')
+def findpeople():
+    
+    driver = webdriver.Chrome('/Users/sungmin/chromedriver')
+    driver.get('http://www.gdlibrary.or.kr/gilib/1000658/100049/bbsList.do')
+    
+    url = 'http://www.gdlibrary.or.kr/gilib/1000658/100049/bbsList.do'
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    nowday = "2020-08-27"
+    rank= 0
+    for tag in soup.select('td'):
+        if tag.text == nowday:
+            rank=rank+1
+
+    driver.find_element_by_xpath("""//*[@id="listForm"]/div[2]/p/span[2]/a""").click()
+    for tag in soup.select('td'):
+        if tag.text == nowday:
+            rank=rank+1
+
+    for page in range(4,14):
+        driver.find_element_by_xpath("""//*[@id="listForm"]/div[2]/p/span[""" +str(page)+"""]/a""").click()
+        for tag in soup.select('td'):
+            if tag.text == nowday:
+                rank=rank+1
+    print("어제의 안심도서대출은 "+str(rank) +"명 입니다.")
 
 
+findpeople()
 
-rank= 1
+
+""""
+//*[@id="listForm"]/div[2]/p/span[4]/a
+//*[@id="listForm"]/div[2]/p/span[5]/a
+//*[@id="listForm"]/div[2]/p/span[6]/a
+//*[@id="listForm"]/div[2]/p/span[7]/a
+//*[@id="listForm"]/div[2]/p/span[8]/a
+//*[@id="listForm"]/div[2]/p/span[12]/a
+//*[@id="listForm"]/div[2]/p/span[4]/a
+rank= 0
 now = datetime.datetime.now()
 nowday1 = (str(now.year)+"-"+str(now.month)+"-"+str(now.day))
-nowday = "2020-08-27"
+nowday = "2020-08-28"
+"""
 
-for tag in soup.select('td'):
-    if nowday == tag.text:
-        rank=rank+1
-    
-for page in soup.select('a'):
-    print(page.text)
-
-#<a href="#" onclick="goPage(2); return false;">2</a>
-print("오늘의 안심도서대출은 "+str(rank) +"명 입니다.")
